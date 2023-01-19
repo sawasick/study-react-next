4eaf8824fc2cf68c7b75ab3d2943491e531d4fcf
sawasick <50393229+sawasick@users.noreply.github.com>
2023-01-19 23:29:30 +0900

    update:map勉強

--- components/Links.js
+++ components/Links.js
@@ -1,8 +1,31 @@
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import { Inter, Tienne, Titillium_Web } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
+
+const ITEMS = [
+  {
+    href: "https://nextjs.org/docs",
+    title: "Docs \u2192",
+    description: "Find in-depth information about Next.js features and&nbsp;API.",
+  },
+  {
+    href: "https://nextjs.org/learn",
+    title: "Learn \u2192",
+    description: "Learn about Next.js in an interactive course with&nbsp;quizzes!",
+  },
+  {
+    href: "https://nextjs.org/templates",
+    title: "Templates \u2192",
+    description: "Discover and deploy boilerplate example Next.js&nbsp;projects.",
+  },
+  {
+    href: "https://nextjs.org/new",
+    title: "Deploy \u2192",
+    description: "Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel.",
+  }
+];
 
 export function Links() {
   return (
@@ -28,7 +51,18 @@ export function Links() {
       </div>
 
       <div className={styles.grid}>
-        <a
+        {ITEMS.map(item => {
+          return (
+              <a key={item.href} href={item.href} className={styles.card} target="_blank" rel="noopener noreferrer">
+                <h2 className={inter.className}>{item.title}</h2>
+                <p className={inter.className}>
+                  {item.description}
+                </p>
+              </a>
+          );
+        })}
+
+        {/* <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           className={styles.card}
           target="_blank"
@@ -83,7 +117,7 @@ export function Links() {
             Instantly deploy your Next.js site to a shareable URL
             with&nbsp;Vercel.
           </p>
-        </a>
+        </a> */}
       </div>
     </>
   )
